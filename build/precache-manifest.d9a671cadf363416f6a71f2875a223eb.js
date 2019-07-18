ba36065d8cab1f880d3e217464ea4dbdbda01cdf
denys tsymbalenko <den_cimbal@mail.ru>
2019-07-18 21:08:15 +0300

    add gitignore and update package.json

---
+++ build/precache-manifest.d9a671cadf363416f6a71f2875a223eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa3c131f897fb0f40eb393caf9abe1d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11303f45a6f675ad4558",
+    "url": "/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "0832aa6d9ea38a35730b",
+    "url": "/static/js/2.2e26626e.chunk.js"
+  },
+  {
+    "revision": "11303f45a6f675ad4558",
+    "url": "/static/js/main.6d0b07a3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
