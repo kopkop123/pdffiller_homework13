a2da2b7a8efaa1c28ecf2439e1bb2aa9abdd019b
denys tsymbalenko <den_cimbal@mail.ru>
2019-07-18 21:24:52 +0300

    update package.json

---
+++ build/precache-manifest.7e29d7c5fa2f0a11fbc7859930142c54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d111719790649cd9707ab221616ba2d",
+    "url": "/pdffiller_homework13/build/index.html"
+  },
+  {
+    "revision": "11303f45a6f675ad4558",
+    "url": "/pdffiller_homework13/build/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "0832aa6d9ea38a35730b",
+    "url": "/pdffiller_homework13/build/static/js/2.2e26626e.chunk.js"
+  },
+  {
+    "revision": "11303f45a6f675ad4558",
+    "url": "/pdffiller_homework13/build/static/js/main.6d0b07a3.chunk.js"
+  },
+  {
+    "revision": "2e479b776e1bd89acf17",
+    "url": "/pdffiller_homework13/build/static/js/runtime~main.f9fc702b.js"
+  }
+]);
